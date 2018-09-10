d2cf682d202bbbb9de5871379bead39312b3c500
S.M. Farhan Shahoriar <farhanshahoriar@gmail.com>
2018-09-10 12:45:57 +0600

    Array lab 1st part added

---
+++ ArrayLab1.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class ArrayLab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int SIZE=55;
+		int[] array=new int [SIZE];
+		int[] runSize =new int [SIZE];
+		int i,j;
+		Random rand=new Random();
+		System.out.println("Printing Array Noramlly: ");
+		for( i=0;i<SIZE;i++) {
+			array[i]=rand.nextInt(80)%10;
+			System.out.print(array[i]+" ");	
+		}
+		System.out.println("");
+		for(i=0;i<SIZE;i++) {
+			for(j=i+1;j<SIZE;j++) {
+				if(array[i]!=array[j])break;
+			}
+			runSize[i]=j-i;
+		}
+		System.out.println("Printing Array Selecting Runs: ");
+		
+		for(i=0;i<SIZE;i++) {
+			if(runSize[i]>1) {
+				System.out.print("(");
+				for(j=i;j<i+runSize[i];j++) {
+					System.out.print(array[j]+" ");
+				}
+				i=j;
+				System.out.print(")");
+			}
+			else System.out.print(array[i]+" ");
+		}
+		
+	}
+
+}
